eecb07f3f8a875003871c3a83af660b59309c6a5
Frank LoTurco <floturcocantsee@protonmail.com>
2024-11-26 17:13:05 -0500

    Add Xwayland dependency.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir ./iso-output
 sudo docker run --rm --privileged --volume ./iso-output:/build-container-installer/build --pull=always \
 ghcr.io/jasonn3/build-container-installer:latest \
+VERSION=41 \
 IMAGE_REPO=ghcr.io/floturcocantsee \
 IMAGE_NAME=kiosk-os \
-IMAGE_TAG=latest \
-VARIANT=CoreOS
+IMAGE_TAG=latest
